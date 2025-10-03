7d9aae91fcc7bdae5540afe5938af034fbbbb3bc
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2025-10-03 18:11:17 +0530

    Day-17

--- DAY-17/reverse_array.cpp
+++ DAY-17/reverse_array.cpp
@@ -4,36 +4,55 @@
 using namespace std;
 
 void reverseArray(vector<int> &v) {
+    
     int start = 0, end = v.size() - 1;
+
     while(start < end) {
+       
         swap(v[start], v[end]);
         start++;
         end--;
+
     }
+
 }
 
 void printArray(vector<int> v) {
+
     for(int i = 0; i < v.size(); i++) {
+
         cout << v[i] << " ";
+
     }
+
     cout << endl;
+
 }
 
 int main() {
+
     vector<int> v;
     int n;
+
     cout << "Enter the number of elements: ";
     cin >> n;
+
     cout << "Enter elements of array: ";
     for(int i = 0; i < n; i++) {
+
         int x;
         cin >> x;
         v.push_back(x);
+
     }
+
     cout << "Original array: ";
     printArray(v);
+
     reverseArray(v);
     cout << "Reversed array: ";
     printArray(v);
+
     return 0;
+    
 }
