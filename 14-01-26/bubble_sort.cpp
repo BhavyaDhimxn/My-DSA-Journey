90520d21de4ae5b6c2fe19c4373b9e4286c39e20
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-01-16 15:25:51 +0530

    16-01-26

--- 14-01-26/bubble_sort.cpp
+++ 14-01-26/bubble_sort.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void selectionSort(int arr[], int n) {
 
-    for(int i = n - 1; i >= 0; i--) {
+    for(int i = n - 1; i > 0; i--) {
 
-        for(int j = 0; j <= i-1; j++) {
+        for(int j = 0; j <= i - 1; j++) {
 
             if(arr[j] > arr[j + 1])
             swap(arr[j], arr[j + 1]);
