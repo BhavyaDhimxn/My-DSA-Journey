cc8b3b56945aab4171c0519ba48fcb07a5943a31
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-04-09 15:51:01 +0530

    09-04-26

--- 09-04-26/move_0.cpp
+++ 09-04-26/move_0.cpp
@@ -2,29 +2,22 @@
 using namespace std;
 
 vector<int> moveZeroes(vector<int> &nums, int n) {
-
-    vector<int> temp;
-
+    int  j = -1;
     for(int i = 0; i < n; i++) {
 
-        if(nums[i] != 0) {
+        if(nums[i] == 0) {
 
-            temp.push_back(nums[i]);
+            j = i;
+            break;
 
         }
-
     }
+    for(int i = j + 1; i < n; i++) {
 
-    for(int i = 0; i < temp.size(); i++) {
-
-        nums[i] = temp[i];
-
-    }
-
-    for(int i = temp.size(); i < n; i++) {
-
-        nums[i] = 0;
-
+        if(nums[i] != 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
     }
 
     return nums;
