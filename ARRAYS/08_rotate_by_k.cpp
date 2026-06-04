480ee6bb4d1fde7de15ff52e4dae130075712152
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-06-05 00:20:29 +0530

    04-06-26

--- ARRAYS/08_rotate_by_k.cpp
+++ ARRAYS/08_rotate_by_k.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 /* 
 BRUTE FORCE METHOD:
+
 void rotateArray(vector<int> &nums, int n, int k) {
 
     k %= n;
@@ -25,6 +27,36 @@ void rotateArray(vector<int> &nums, int n, int k) {
 }
 */
 
+/*
+BETTER - 1 APPROACH:
+
+void rotateArray(vector<int> &nums, int n, int k) {
+
+    k %= n;
+    int temp[k];
+
+    for(int i = 0; i < k; i++) {
+
+       temp[i] = nums[i];
+
+    }
+    for(int i = k; i < n; i++) {
+
+        nums[i - k] = nums[i];
+
+    }
+    for(int i = (n - k); i < n; i++) {
+
+        nums[i] = temp[i - (n - k)];
+
+    }
+
+}
+*/
+
+/*
+BETTER - 2 APPROACH:
+
 void rotateArray(vector<int> &nums, int n, int k) {
 
     vector<int> temp(n);
@@ -36,6 +68,17 @@ void rotateArray(vector<int> &nums, int n, int k) {
     }
 
     nums = temp;
+
+}
+*/
+
+void rotateArray(vector<int> &nums, int n, int k) {
+
+    k %= n;
+
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+    reverse(nums.begin(), nums.end());
 
 }
 
