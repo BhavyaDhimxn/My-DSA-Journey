e1ae8b87e68d1fceba7fc2021e624f4325042038
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-06-08 23:16:52 +0530

    08-06-26

---
+++ ARRAYS/12_intersection_sorted_arrays.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+/*
+BRUTE FORCE APPROACH:
+*/
+vector<int> unionArrays(vector<int> &num1, vector<int> &num2, int n1, int n2) {
+    
+    vector<int> temp;
+    vector<int> vis(n2, 0);
+
+    for(int i = 0; i < n1; i++) {
+
+        for(int j = 0; j < n2; j++) {
+
+            if(num1[i] == num2[j]) {
+
+                temp.push_back(num1[i]);
+                vis[j] = 1;
+                break;
+            }
+
+            if(num2[j] > num1[i]) break;
+            
+        }
+    }
+    return temp;
+
+}
+
+
+/*
+OPTIMAL APPROACH:
+*/
+// vector<int> unionArrays(vector<int> &num1, vector<int> &num2, int n1, int n2) {
+
+//     vector<int> temp;
+//     int i = 0, j = 0;
+
+//     while(i < n1 && j < n2) {
+
+//         if(num1[i] <= num2[j]) {
+//             if(temp.size() == 0 || temp.back() != num1[i]) {
+//                 temp.push_back(num1[i]);
+//             }
+//             i++;
+//         }
+//         else {
+//             if(temp.size() == 0 || temp.back() != num2[j]) {
+//                 temp.push_back(num2[j]);
+//             }
+//             j++;
+//         }
+//     } 
+
+//     while(i < n1) {
+//         if(temp.size() == 0 || temp.back() != num1[i]) {
+//                 temp.push_back(num1[i]);
+//             }
+//             i++;
+//     }
+
+//     while(j < n2) {
+//         if(temp.size() == 0 || temp.back() != num2[j]) {
+//                 temp.push_back(num2[j]);
+//             }
+//             j++;
+//     }
+
+//     return temp;
+// }
+
+int main() {
+
+    int n1, n2;
+
+    cout << "Enter the size of first array: ";
+    cin >> n1;
+    vector<int> num1(n1);
+    cout << "Enter the elements of first array: ";
+    for(int i = 0;i < n1; i++) {
+
+        cin >> num1[i];
+
+    }
+
+    cout << "Enter the size of second array: ";
+    cin >> n2;
+    vector<int> num2(n2);
+    cout << "Enter the elements of second array: ";
+    for(int i = 0;i < n2; i++) {
+
+        cin >> num2[i];
+
+    }
+
+    cout << "The union array is: ";
+    
+    vector<int> nums = unionArrays(num1, num2, n1, n2);
+    for(int x : nums) {
+    cout << x << " ";
+    }
+    return 0;
+}
