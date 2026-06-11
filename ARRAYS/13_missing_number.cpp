11e714a42ffb1b05e402f9f1418be508dc55db12
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-06-11 14:36:05 +0530

    11-06-26

--- ARRAYS/13_missing_number.cpp
+++ ARRAYS/13_missing_number.cpp
@@ -58,6 +58,25 @@ int missingNumber(vector<int> &nums, int N, int n) {
 }
 */
 
+
+int missingNumber(vector<int> &nums, int N, int n) {
+
+    int xor1 = 0;
+    int xor2 = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        xor1 ^= i+1;
+        xor2 ^= nums[i];
+
+    }
+
+    xor1 ^= N;
+
+    return xor1^xor2;
+
+}
+
 int main() {
 
     int N;
