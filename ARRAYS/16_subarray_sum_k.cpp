fc881f25651cdb16fccd69e90b24cc3e350ee51b
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-06-15 20:29:51 +0530

    15-06-26

---
+++ ARRAYS/16_subarray_sum_k.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+/*
+BRUTE FORCE APPROACH:
+*/
+int longestSubarray(vector<int> &nums, int n, int k) {
+    int len = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        int sum = 0;
+
+        for(int j = i; j < n; j++) {
+
+            sum += nums[j];
+
+            if(sum == k) {
+
+                len = max(len, j - i + 1);
+
+            }
+        }
+
+    }
+
+    return len;
+}
+
+
+
+/*
+BETTER APPROACH:
+
+int longestSubarray(vector<int> &nums, int n) {
+
+    map<long long, int> mpp;
+
+    for(int i = 0; i < n; i++) {
+
+        mpp[nums[i]]++;
+
+    }
+
+    for(auto it : mpp) {
+
+        if(it.second == 1) {
+
+            return it.first;
+
+        }
+    }
+
+    return -1;
+
+}
+*/
+
+/*
+OPTIMAL APPROACH:
+
+int longestSubarray(vector<int> &nums, int n, int k) {
+
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        ans = ans^nums[i];
+
+    }
+
+    return ans;
+}
+*/
+
+int main() {
+
+    int n;
+    cout << "Enter the size of your array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements in your array: ";
+    for(int i = 0; i < n; i++) {
+
+        cin >> nums[i];
+
+    }
+    
+    int k;
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+    int result = longestSubarray(nums, n, k);
+
+    cout << "The length of the longest subarray is: " << result << endl;
+    
+    return 0;
+
+}
