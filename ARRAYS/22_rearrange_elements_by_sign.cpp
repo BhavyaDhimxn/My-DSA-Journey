2439c7da9b1b2c345726b2c52d258b2db697113d
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-06-19 18:44:15 +0530

    19-06-26

--- ARRAYS/22_rearrange_elements_by_sign.cpp
+++ ARRAYS/22_rearrange_elements_by_sign.cpp
@@ -35,7 +35,7 @@ vector<int> rearrangeElements(vector<int> &nums, int n) {
 
 /*
 OPTIMAL APPROACH:
-*/
+
 vector<int> rearrangeElements(vector<int>& nums, int n) {
         
     vector<int> temp(n, 0);
@@ -61,8 +61,59 @@ vector<int> rearrangeElements(vector<int>& nums, int n) {
     return temp;
 
 }
+*/
 
+vector<int> rearrangeElements(vector<int> &nums, int n) {
+    
+        vector<int> pos, neg;
 
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] > 0) {
+
+                pos.push_back(nums[i]);
+
+            } 
+            else {
+
+                neg.push_back(nums[i]);
+
+            } 
+
+        }
+        
+        int index = 0;
+
+        for(int i = 0; i < pos.size() && i < neg.size(); i++) {
+
+            nums[2*i] = pos[i];
+            nums[2*i+1] = neg[i];
+            index++;
+
+        }
+
+        int k = index*2;
+        if(pos.size() > neg.size()) {
+
+            for(int i = neg.size(); i < pos.size(); i++) {
+
+                nums[k++] = (pos[i]);
+
+            }
+            
+        }
+        else {
+
+            for(int i = pos.size(); i < neg.size(); i++) {
+
+                nums[k++] = (neg[i]);
+
+            }
+
+        }    
+
+        return nums;
+}
 
 int main() {
 
