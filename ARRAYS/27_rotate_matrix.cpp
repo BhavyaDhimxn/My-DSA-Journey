4a2b05450c8168c48484bbb234be43c00d8532ae
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-07-21 22:03:54 +0530

    21-07-26

--- ARRAYS/27_rotate_matrix.cpp
+++ ARRAYS/27_rotate_matrix.cpp
@@ -4,7 +4,27 @@
 
 using namespace std;
 
+/*
+BRUTE APPROACH
+void rotate(vector<vector<int>>& matrix) {
 
+    int n = matrix.size();
+
+    vector<vector<int>> temp(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++) {
+
+        for(int j = 0; j < n; j++) {
+
+            temp[j][n - 1 - 1] = matrix[i][j];
+
+        }
+    }
+
+    matrix = temp;
+
+}
+*/
 
 /*
 OPTIMAL APPROACH
