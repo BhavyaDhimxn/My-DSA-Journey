90ceca0de4757c3b7b87e8b35673463e47a558a0
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-08-03 17:47:16 +0530

    03-08-26

--- ARRAYS/35_3_sum.cpp
+++ ARRAYS/35_3_sum.cpp
@@ -55,6 +55,42 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 }
 */
 
+/*
+OPTIMAL APPROACH
+*/
+vector<vector<int>> threeSum(vector<int>& nums) {
+
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+
+    for(int i = 0; i < n; i++) {
+
+        if(i > 0 && nums[i - 1] == nums[i]) continue;
+        int j = i + 1;
+        int k = n - 1;
+
+        while(j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0) {
+                j++;
+            }
+            else if(sum > 0) {
+                k--;
+            }
+            else{
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j < k && nums[j - 1] == nums[j]) j++;
+                while(j < k && nums[k + 1] == nums[k]) k--;
+            }
+        } 
+    }
+    return ans;
+}
+
 int main() {
     int n;
     cout << "Enter the number of elements: ";
