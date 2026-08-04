537b7d722b26d9c9754ab7ec35e2aa30b662e26e
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-08-04 18:16:41 +0530

    04-08-26

--- ARRAYS/39_merge_sorted_arrays.cpp
+++ ARRAYS/39_merge_sorted_arrays.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-
+/*
+BRUTE FORCE
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     
     vector<int> nums3; 
@@ -32,7 +33,34 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         nums1[i] = nums3[i];
     }
 }
+*/
 
+
+/*
+OPTIMAL APPROACH
+*/
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;         
+    int j = n - 1;         
+    int k = m + n - 1;     
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
 
 int main() {
     int m, n;
