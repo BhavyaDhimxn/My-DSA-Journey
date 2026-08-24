bba6c29fc48a26d71cdaec1e50350f1a61b32074
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-08-24 17:42:02 +0530

    24-08-26

--- BINARY SEARCH/find_the_sqrt.cpp
+++ BINARY SEARCH/find_the_sqrt.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 /*
 BRUTE FORCE APPROACH:
-*/
+
 int findSqrt(int num) {
     int i = 1;
     while(i <= num) {
@@ -12,28 +12,24 @@ int findSqrt(int num) {
     }
     return i - 1;
 }
-/*
-int findSqrt(int num) {
-    // Base cases for 0 and 1
-    if (num == 0 || num == 1) return num;
+*/
 
-    int low = 1, high = num, ans = 0;
+/*
+OPTIMAL APPROACH:
+*/
+int findSqrt(int num) {
+    int low = 1, high = num;
 
     while (low <= high) {
-        // Prevent potential integer overflow
         int mid = low + (high - low) / 2;
-
-        // Use division to prevent mid * mid integer overflow
         if (mid <= num / mid) {
-            ans = mid;      // Store the closest floor value found so far
             low = mid + 1;  // Look for a larger match
         } else {
             high = mid - 1; // Look for a smaller match
         }
     }
-    return ans;
+    return high;
 }
-*/
 
 int main() {
     int number;
