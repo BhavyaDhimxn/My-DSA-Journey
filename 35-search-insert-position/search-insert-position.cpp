1fe6dbe1263280e070b5af664c48832cb8f5e69c
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-08-31 21:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.22%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -22,9 +22,8 @@ public:
         while(low <= high) {
             int mid = (low + high)/2;
 
-            if (nums[mid] == target) return mid;
-            else if(nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
+            if(nums[mid] >= target) high = mid - 1;
+            else low = mid + 1;
         }
         return low;
     }
