4025ef18beb73063e45c0c4f77d973be89d7d45f
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-08-31 21:13:07 +0530

    31-08-26

--- BINARY SEARCH/02-lower-bound.cpp
+++ BINARY SEARCH/02-lower-bound.cpp
@@ -6,13 +6,9 @@ using namespace std;
 LINEAR SEARCH
 
 int lowerBound(vector<int>& nums, int n, int x) {
-
     for(int i = 0; i < n; i++) {
-
         if(nums[i] >= x) return i;
-
     }
-
     return n;
 }
 */
@@ -21,24 +17,15 @@ int lowerBound(vector<int>& nums, int n, int x) {
 BINARY SEARCH
 */
 int lowerBound(vector<int>& nums, int n, int x) {
-
-    int low = 0;
-    int high = n - 1;
-    int ans = n;
+    int low = 0, high = n - 1;
 
     while(low <= high) {
-        
         int mid = (low + high)/2;
 
-        if(nums[mid] >= x){
-           ans = mid;
-           high = mid - 1;  
-        }
+        if(nums[mid] >= x) high = mid - 1;
         else low = mid + 1;
-
     }
-
-    return ans;
+    return low;
 }
 
 
