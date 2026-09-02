5029f25e28911d31fa2d76741dfe85dc680b4699
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-02 17:52:53 +0530

    Time: 76 ms (18.32%) | Memory: 18.8 MB (39.30%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /*
     BRUTE FORCE APPROACH:
-    */
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
@@ -14,11 +14,11 @@ public:
         }
         return false;
     }
-    
+    */
 
     /*
     BETTER APPROACH:
-    
+    */
     bool binarySearch(vector<int> &nums, int target) {
         int n = nums.size();
         int low = 0, high = n - 1;
@@ -36,13 +36,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int ans = false;
 
         for(int i = 0; i < n; i++) {
-            if(matrix[i][m - 1] >= target) return binarySearch(matrix[i], target);
+            if(matrix[i][m - 1] >= target) {
+                ans = binarySearch(matrix[i], target);
+                if(ans == true) return ans;
+            } 
         }
-        return false;
+        return ans;
     }
-    */
+    
 
     /*
     OPTIMAL APPROACH:
