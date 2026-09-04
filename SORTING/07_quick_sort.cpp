60d6b096018a74aa9a30bc2252b99dc7db02cc96
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-04 23:31:42 +0530

    04-09-26

--- SORTING/07_quick_sort.cpp
+++ SORTING/07_quick_sort.cpp
@@ -4,55 +4,33 @@
 using namespace std;
 
 int partition(vector<int> &nums, int low, int high) {
-
     int pivot = nums[low];
     int i = low;
     int j = high;
 
     while(j > i) {
-
-        while(nums[i] <= pivot && i < high){
-
-            i++;
-
-        }
-        while(nums[j] > pivot && j > low){
-
-            j--;
-
-        }
-
+        while(nums[i] <= pivot && i < high) i++;
+        while(nums[j] > pivot && j > low) j--;
         if(i < j) swap(nums[i], nums[j]);
-
     }
-
     swap(nums[low], nums[j]);
     return j;
-
 }
 
 void quickSort(vector<int> &nums, int low, int high) {
-
     if(low < high) {
-
         int pIndex  = partition(nums, low, high);
+
         quickSort(nums, low, pIndex - 1);
         quickSort(nums, pIndex + 1, high);
-
     }
-
 }
 
 void printArray(vector<int> &nums, int n) {
-
     for(int i = 0; i < n; i++) {
-
         cout << nums[i] << " ";
-
     }
-
     cout << endl;
-
 }
 int main() {
 
@@ -63,9 +41,7 @@ int main() {
     vector<int> nums(n);
     cout << "Enter the elements in your array: ";
     for(int i = 0; i < n; i++) {
-
         cin >> nums[i];
-
     }
 
     quickSort(nums, 0, n - 1);
