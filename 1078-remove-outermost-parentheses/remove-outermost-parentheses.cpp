eb937d972d75c977041d4e7212182999a706f278
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-09 18:19:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (22.71%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.length();
+        int counter = 0;
+        string ans;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == ')') counter--;
+            if(counter != 0) ans.push_back(s[i]);
+            if(s[i] == '(') counter++;
+        }
+        return ans;
+    }
+};
