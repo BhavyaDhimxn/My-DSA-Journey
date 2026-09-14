5039ea3a11a75ce30a4ebd892d25042587a8e7a0
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-14 18:04:44 +0530

    14-09-26

--- SORTING/01-selection-sort.cpp
+++ SORTING/01-selection-sort.cpp
@@ -3,19 +3,25 @@
 using namespace std;
 
 void selectionSort(int arr[], int n) {
+    //Outer loop -> Reduces range by 1 element from left in every iteration as leftmost(minimum) element gets correctly placed.
+    //Outer loop -> From 0 to (n - 2), as last element is already sorted after (n - 2) iteration.
     for(int i = 0; i < n - 1; i++) {
-        //Store the minimum index in the current range.
+        //Initialise a variable with the leftmost(minimum) index as just decalaration gives it garbage value.
+        //Case: If no smaller element is found, the leftmost element will be swapped with variable containing garbage value.
         int minimum = i;
-        //Retreive the actual minimum index.
+        //Inner loop -> From (i + 1) to n - 1.
+        //Start != 0 as in 2nd iteration 0th element is correctly placed.
+        //Start != i as it is taken as the minimum, no need to compare it with itself.
         for(int j = i + 1; j < n; j++) {
-            if(arr[j] < arr[j - 1]) {
-                minimum = j;
-            }
+            //Check for global minimum in the current range. 
+            //Check every element in the range against our assumed minimum and updated the minimum correctly.
+            if(arr[j] < arr[minimum]) minimum = j;
         }
-        //Swap assumed minimum element(1st in the range) with actual one.
-        swap(arr[minimum], arr[i]);
+        //Swap assumed minimum element with actual minimum element.
+        swap(arr[i], arr[minimum]);
     }
 }
+
 
 void printArray(int arr[], int n) {
     for(int i = 0; i < n; i++) {
