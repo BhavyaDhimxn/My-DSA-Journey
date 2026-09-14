7957630fa946cf21fd1396652dd96c073d316c39
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-14 18:29:26 +0530

    14-09-26

--- STRINGS/01-(1078)-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ STRINGS/01-(1078)-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -4,15 +4,24 @@ public:
     OPTIMAL APPROACH:
     */
     string removeOuterParentheses(string s) {
+        //Initialise a variable with length of string(to use in loop).
         int n = s.length();
-        int counter = 0;
-        string ans;
+        //Initialise a variable with 0(when count = 0 -> outermost parentheses)to keep count in every iteration.
+        int count = 0;
+        //Declare an empty string to store final answer.
+        string ans = "";
 
+        //Loop to iterate over every element in string and update ans.
         for(int i = 0; i < n; i++) {
-            if(s[i] == ')') counter--;
-            if(counter != 0) ans.push_back(s[i]);
-            if(s[i] == '(') counter++;
+            //Check for ")" first. If true decrease count.
+            //Case: "(" checked first and it is true. If it happens to be outermost parentheses, count will be ++ and it will get included in the answer.
+            if(s[i] == ')') count--;
+            //Check for count number. Whenever count is non zero -> element added.
+            if(count != 0) ans += s[i];
+            //Check for "(". If true increase count.
+            if(s[i] == '(') count++;
         }
+        //Return the update string stored in ans.
         return ans;
     }
 };
