5d84365c397cb4d2ac9c0a18366519d23b5bcc67
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-14 20:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (52.25%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
 
     /*
     BRUTE FORCE APPROACH:
-    */
+   
     string reverseWords(string s) {
         //Declare a list(vector) of string data type to store non-space sequences(word).
         vector<string> words;
@@ -35,5 +35,35 @@ public:
         }
         //Return the final result.
         return result;
+    }
+    */
+
+    /*
+    OPTIMAL APPROACH:
+    */
+    string reverseWords(string s) {
+        //Reverse the entire string before extraction(we will get correct order of words, just in reverse)
+        reverse(s.begin(), s.end());
+        //Initialise string result that stores final answer.
+        string result = "";
+
+        //Outer loop -> Each iteration is one word made and added to the result.
+        for(int i = 0; i < s.length(); i++) {
+            //Initialise strin word inside the outer loop as we need it to reset after every word.
+            string word = "";
+
+            //Inner loop -> To identify words, runs till the end of s and till " " is found.
+            while(i < s.length() && s[i] != ' ') {
+                word += s[i];
+                //Increment i for every next char to be checked;
+                i++;
+            }
+            //reverse the word.
+            reverse(word.begin(), word.end());
+            //Add a space and the word if there is a word(to skip leading spaces).
+            if(!word.empty()) result += ' ' + word;
+        }
+        //Return the substring(1) of result as 1st char in result will always be a ' ', due to our logic in for loop, for addition of words to result.
+        return result.substr(1);
     }
 };
