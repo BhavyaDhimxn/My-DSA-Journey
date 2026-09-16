85722e6c30512b3691ce5c02e3585de2a51b431f
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-16 22:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,39 +3,44 @@ public:
 
     /*
     BRUTE FORCE APPROACH:
-    */
+    
     bool isAnagram(string s, string t) {
+        //Edge case -> If srings have unmatched length, they will never be anagram of each other.
         if(s.length() != t.length()) return false;
-
+        //Sort both the strings.
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-
+        //After sorting if both strings are unequal -> can't be anagrams
         if(s != t) return false;
-        // for(int i = 0; i < s.length(); i++) {
-        //     freq[s[i] - 'a']++;
-        //     freq[t[i] - 'a']--;
-        // }
-        // for(auto it : freq) {
-        //     if(it != 0) return false;
-        // }
+        
         return true;
     }
+    */
+
     /*
     OPTIMAL APPROACH:
-
+    */
     bool isAnagram(string s, string t) {
+        //Edge case -> If srings have unmatched length, they will never be anagram of each other.
         if(s.length() != t.length()) return false;
-
+        //Create an integer array with length of number of alphabets, initialised with 0s.
+        //To increment/decrement the number of occurance of an alphabet.
         int freq[26] = {0};
 
+        //Loop -> from 0 to either of their lengths.
+        //If anagram -> all elements in array will = 0. 
+        //As equal number of individual alphabets cancel each other.
         for(int i = 0; i < s.length(); i++) {
+            //Increment the index(ith alphabet) which has ith position in s.
             freq[s[i] - 'a']++;
+            //decrement the index(ith alphabet) which has ith position in s.
             freq[t[i] - 'a']--;
         }
+        //Iterate over the array to confirm.
         for(auto it : freq) {
             if(it != 0) return false;
         }
         return true;
     }
-    */
+    
 };
