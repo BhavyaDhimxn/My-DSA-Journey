7b4971513abc163f58619bcee4e04331bc6c351f
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-16 21:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (23.59%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+    /*
+    BRUTE FORCE APPROACH:
+
     bool rotateString(string s, string goal) {
         //Edge case -> If srings have unmatched length, they will never be isomorphic.
         if(s.length() != goal.length()) return false;
@@ -11,5 +15,19 @@ public:
             rotate(s.begin(), s.begin() + 1, s.end());
         }
         return false;
+    }
+    */
+
+    /*
+    OPTIMAL APPROACH:
+    */
+    bool rotateString(string s, string goal) {
+        //Edge case -> If srings have unmatched length, they will never be isomorphic.
+        if(s.length() != goal.length()) return false;
+        //Create a string with s concatenated with itself -> it will conatin all the substrings
+        //Rotated string will always be a substring of doubleS.
+        string doubleS = s + s;
+        //Return true if goal is found and false if not.
+        return doubleS.find(goal) != string::npos;
     }
 };
