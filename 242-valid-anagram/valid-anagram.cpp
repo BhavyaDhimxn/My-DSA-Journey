da6f717a8fd6ccf196655e0907d35acc9ac7216d
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-16 22:33:49 +0530

    Time: 3 ms (44.99%) | Memory: 9.7 MB (48.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+
+    /*
+    BRUTE FORCE APPROACH:
+    */
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s != t) return false;
+        // for(int i = 0; i < s.length(); i++) {
+        //     freq[s[i] - 'a']++;
+        //     freq[t[i] - 'a']--;
+        // }
+        // for(auto it : freq) {
+        //     if(it != 0) return false;
+        // }
+        return true;
+    }
+    /*
+    OPTIMAL APPROACH:
+
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
 
@@ -14,4 +37,5 @@ public:
         }
         return true;
     }
+    */
 };
