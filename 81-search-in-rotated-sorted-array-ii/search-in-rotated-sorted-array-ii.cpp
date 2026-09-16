59371f203be33ea6a4e31d440aa579e0c2eb15f8
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-16 17:14:52 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (80.61%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -13,13 +13,15 @@ public:
                 high = high - 1;
                 continue;
             }
-            //If left half is sorted:
+            //If left half is sorted: when lowest element in <= element on mid
             if(nums[low] <= nums[mid]) {
+                //Check if the target lies in this half.
                 if(target <= nums[mid] && target >= nums[low]) high = mid - 1;
                 else low = mid + 1;
             }
-            //If right half is sorted:
+            //If right half is sorted: when element on mid + 1 <= highest element.
             else {
+                //Check if the target lies in this half.
                 if(target >= nums[mid] && target <= nums[high]) low = mid + 1;
                 else high = mid - 1;
             }
