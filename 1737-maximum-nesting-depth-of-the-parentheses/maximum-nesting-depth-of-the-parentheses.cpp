fe107033302c4c5693f28c50d5e97e1ca7fd2a00
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-17 17:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (99.12%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int maxDepth(string s) {
-        int count = 0, maximum = 0;
 
+    /*
+    OPTIMAL APPROACH:
+    */
+    int maxDepth(string s) {
+        //initialise 2 variables 1. To store the count of parantheses 2. To store the maximum count.
+        int count = 0, maximum = 0;
+        
+        //Loop -> Runs from 0 - last char in string.
         for(int i = 0; i < s.length(); i++) {
+            //Increase count every time '(' is encountered
+            //Store this count as a possible maximum count.
             if(s[i] == '(') {
                 count++;
                 maximum = max(maximum, count);
             }
+            //Else if ')' encountered decrease the count.
+            //If valid parentheses string -> Parenthesis always in pairs(even) -> will cancel each other.
             else if(s[i] == ')') count--;
+            //If none of the above just continue through the loop. 
         }
+        //Return the maximum value stored at the end.
         return maximum;
     }
 };
