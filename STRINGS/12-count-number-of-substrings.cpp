5418aa6a7a5cbe8d7db32f2088c2bb8a2436e19b
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-18 19:00:25 +0530

    18-09-26

---
+++ STRINGS/12-count-number-of-substrings.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int maxSubstrings(string s) {
+    int n = s.length();
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    string s;
+    cout << "Enter your string: ";
+    cin >> s;
+
+    int result = maxSubstrings(s);
+    cout << "The number of substrings in your string is: " << result << endl;
+
+    return 0; 
+}
