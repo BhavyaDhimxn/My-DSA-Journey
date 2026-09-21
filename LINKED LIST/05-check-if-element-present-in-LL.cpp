29ceae9d309deffea129efede9938fab9052c6b2
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-21 18:37:40 +0530

    21-09-26

---
+++ LINKED LIST/05-check-if-element-present-in-LL.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+public:
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+Node* convertArrayToLL(vector<int> nums) {
+    Node* head = new Node(nums[0]);
+    Node* mover = head;
+
+    for(int i = 1; i < nums.size(); i++) {
+        Node* temp = new Node(nums[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+} 
+
+bool traversalLL(Node* head, int val) {
+    //Create a temp node to store head node.
+    Node* temp = head;
+
+    //Loop -> runs while temp is valid/till nodes exist/till the pointer of a node points to null.
+    while(temp) {
+        //pushback the data/val in vector from LL.
+        if(temp->data == val) return true;
+        //Update temp with pointer of next node in each iteration.
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    Node* head = convertArrayToLL(nums);
+    bool ans = traversalLL(head, 4);
+
+    cout << "Does this element exist in LL: " << ans << endl;
+
+    return 0;
+}
