a6bba73e242e89878632768d43278f839518a10c
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2026-09-21 18:28:18 +0530

    21-09-26

---
+++ LINKED LIST/04-count-nodes-in-linked-list.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+public:
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+Node* convertArrayToLL(vector<int> nums) {
+    Node* head = new Node(nums[0]);
+    Node* mover = head;
+
+    for(int i = 1; i < nums.size(); i++) {
+        Node* temp = new Node(nums[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+} 
+
+int traversalLL(Node* head) {
+    //Create a temp node to store head node.
+    Node* temp = head;
+    //Create a vector to return the data in traversed nodes.
+    vector<int> nums;
+    //Keep a count of nodes.
+    int count = 0;
+    //Loop -> runs while temp is valid/till nodes exist/till the pointer of a node points to null.
+    while(temp) {
+        //pushback the data/val in vector from LL.
+        nums.push_back(temp->data);
+        //Update temp with pointer of next node in each iteration.
+        temp = temp->next;
+        //Increment counter in each iteration/for every next node.
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    Node* head = convertArrayToLL(nums);
+    int ans = traversalLL(head);
+
+    cout << "The total number of nodes in LL: " << ans << endl;
+
+    return 0;
+}
